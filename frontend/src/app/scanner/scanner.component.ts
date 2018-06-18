3fee8085263900d84a0f680a72ec86004d563530
Amine JABRI <m.amine.jabri@gmail.com>
2018-06-18 10:42:15 +0200

    some refacto : qr Scanner  front (#26)

--- frontend/src/app/scanner/scanner.component.ts
+++ frontend/src/app/scanner/scanner.component.ts
@@ -17,26 +17,18 @@ export class ScannerComponent implements OnInit {
   ngOnInit() {
     this.qrScannerComponent.getMediaDevices().then(devices => {
       console.log(devices);
-      const videoDevices: MediaDeviceInfo[] = [];
-      for (const device of devices) {
-        if (device.kind.toString() === 'videoinput') {
-          videoDevices.push(device);
-        }
-      }
-      if (videoDevices.length > 0){
-        let choosenDev;
-        for (const dev of videoDevices){
-          if (dev.label.includes('front') || dev.label.includes('Camera')){
-            choosenDev = dev;
-            break;
-          }
-        }
-        if (choosenDev) {
-          this.qrScannerComponent.chooseCamera.next(choosenDev);
+
+      const videoDevices = devices.
+          filter(device => device.kind.toString() === 'videoinput');
+
+      let choosenDevice = videoDevices.
+          filter(device => device.label.includes('front') || device.label.includes('Camera'))[0];
+
+        if (choosenDevice) {
+          this.qrScannerComponent.chooseCamera.next(choosenDevice);
         } else {
           this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
         }
-      }
     });
 
     this.qrScannerComponent.capturedQr.subscribe(result => {
